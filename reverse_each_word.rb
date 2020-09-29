8e1dd50dcf910ea4de1ffe8f5ca5f2c63444ebd0
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-29 20:04:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 #def reverse_each_word(string_sentence)
 #  array = string_sentence.split
-# new_array = []
-# array.each do |element|
-#   new_array << element.reverse 
-# end 
-# new_array.join(" ")
+#  new_array = []
+#  array.each do |element|
+#    new_array << element.reverse 
+#  end 
+#new_array.join(" ")
 #end  
 
 
