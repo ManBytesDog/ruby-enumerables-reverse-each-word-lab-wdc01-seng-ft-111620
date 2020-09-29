73cc3a2e8508e6b29b6bdd94f1aa81b0f452f985
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-29 20:15:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ reverse_each_word(string_sentence)
   array = string_sentence.split
   in_place_reverse_string_sentence = []
     array.collect do |element|
-      
+      in_place_reverse_string_sentence << element.reverse 
     end
   
 end 
