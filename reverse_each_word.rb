a5965c2716ba41bd3066831d8411b9d622209311
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-29 20:00:54 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+#def reverse_each_word(string_sentence)
+#  array = string_sentence.split
+# new_array = []
+# array.each do |element|
+#   new_array << element.reverse 
+# end 
+# new_array.join(" ")
+#end  
+
+
+reverse_each_word(string_sentence)
+  array = string_sentence.split
+  
+end 
